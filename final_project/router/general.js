61e7975bc0356c400a7a0799df7d0a8c8283ab3f
wmhunt1 <wmatthunt@gmail.com>
2025-08-15 11:55:56 -0400

    ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,8 +19,9 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
