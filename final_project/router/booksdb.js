a1326e7e7e759470f8cca1a5ccaf313d369075a0
wmhunt1 <wmatthunt@gmail.com>
2025-08-15 12:40:46 -0400

    idbnE

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,64 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+    1: {
+        "author": "Chinua Achebe",
+        "title": "Things Fall Apart",
+        "reviews": {},
+        "isbn": "9780385474542" // Anchor Books paperback
+    },
+    2: {
+        "author": "Hans Christian Andersen",
+        "title": "Fairy tales",
+        "reviews": {},
+        "isbn": "9780147514781" // Penguin Classics paperback
+    },
+    3: {
+        "author": "Dante Alighieri",
+        "title": "The Divine Comedy",
+        "reviews": {},
+        "isbn": "9780147517171" // Penguin Classics, Pinsky translation
+    },
+    4: {
+        "author": "Unknown",
+        "title": "The Epic Of Gilgamesh",
+        "reviews": {},
+        "isbn": "9780141026284" // Penguin Classics, George translation
+    },
+    5: {
+        "author": "Unknown",
+        "title": "The Book Of Job",
+        "reviews": {},
+        "isbn": "9780393979872" // Norton Critical Edition
+    },
+    6: {
+        "author": "Unknown",
+        "title": "One Thousand and One Nights",
+        "reviews": {},
+        "isbn": "9780393922373" // Norton Critical Edition
+    },
+    7: {
+        "author": "Unknown",
+        "title": "Nj\u00e1l's Saga",
+        "reviews": {},
+        "isbn": "9780140447699" // Penguin Classics
+    },
+    8: {
+        "author": "Jane Austen",
+        "title": "Pride and Prejudice",
+        "reviews": {},
+        "isbn": "9780141439518" // Penguin Classics
+    },
+    9: {
+        "author": "Honor\u00e9 de Balzac",
+        "title": "Le P\u00e8re Goriot",
+        "reviews": {},
+        "isbn": "9780140440256" // Penguin Classics
+    },
+    10: {
+        "author": "Samuel Beckett",
+        "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+        "reviews": {},
+        "isbn": "9780802142273" // Grove Press paperback
+    }
 }
 
 module.exports=books;
